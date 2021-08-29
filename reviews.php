c7a53697d5fbec59b76eeeb89e77921d7cd1af3e
Bianca Romero <bromero@lesley.edu>
2021-08-28 22:49:20 -0400

    Add form tag to reviews.php input field / checkbox

--- reviews.php
+++ reviews.php
@@ -10,11 +10,26 @@
         <div id="form">
                 <h2>Contact Us</h2>
                 <form>
+                    <label for="fname">First name:</label>
+                    <input type="text" id="fname" name="fname"><br><br>
+                    <label for="lname">Last name:</label>
+                    <input type="text" id="lname" name="lname"><br><br>
+                    <label for="review">Review:</label>      <!-- need a bigger text box-->
+                    <input type="text" id="review" name="review"><br><br>
 
-
-
-                
                 </form>
+
+                <form action="/reviews.php" method="get">
+                    <p> <h3> Overall Visit: </h3> </p> 
+                        <br>
+                    <input type="checkbox" name="status1" value="okay">
+                    <label for="vehicle1"> This visit was not good at all. </label><br>
+                    <input type="checkbox" name="status2" value="good">
+                    <label for="vehicle2"> This visist was alright. </label><br>
+                    <input type="checkbox" name="status2" value="great" checked>
+                    <label for="vehicle3"> This visist was great!</label><br><br>
+                    <input type="submit" value="Submit">
+</form>
         </main>
     </div><!--close for main-->
     <?php include 'footer.php' ?>
