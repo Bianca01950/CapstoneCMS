b1177e0c5dc4771f285f4919a9fb178811ba5b38
MEGHAN KINSHERF <kinsherf@lesley.edu>
2021-09-02 22:11:14 -0400

    published site

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-    $dbServer = "localhost";
-    $dbUsername = "root";    // This is the same username as phpMyAdmin
-    $dbPassword = "root";    // This is the same password as phpMyAdmin
-    $dbName = "review";
+    $dbServer = "sql312.byethost12.com";
+    $dbUsername = "b12_29613976";    // This is the same username as phpMyAdmin
+    $dbPassword = "Mypassw0rd";    // This is the same password as phpMyAdmin
+    $dbName = "b12_29613976_reviews";
 
     // Create connection
     $conn = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
